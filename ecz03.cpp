b4f859f34403ce0aef3085a673709f2455ba780c
HOLYSHIELD <157357160+BLVCKDIAMONDSKY@users.noreply.github.com>
2024-02-28 13:07:03 +0300

    Add project files.

---
+++ ecz03.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x = 0;
+	float y;
+	printf("x = ");
+	scanf_s ("%ld", &x);
+
+	y = sqrt(pow(x, 2)) + pow(x, 2);
+
+	printf("y: %ld\n", y);
+}
